e3f8f1680d7bf5e8b58c8a1c1dafbbd3eab2f89e
Partha Chowdhury <partha.c@aqbsolutions.com>
2016-09-23 17:46:50 +0530

    sql file change to tutorial

--- names.sql
+++ names.sql
@@ -1,63 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 4.5.2
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Sep 20, 2016 at 04:44 PM
--- Server version: 10.1.13-MariaDB
--- PHP Version: 5.5.37
+Drop table if exists customers;
+CREATE TABLE customers(
+  id CHAR (32) NOT NULL,
+  PRIMARY KEY(id),
+  first_name VARCHAR(64),
+  last_name VARCHAR(64),
+  email VARCHAR(64)
+);
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `chef_sample`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `names`
---
-
-CREATE TABLE `names` (
-  `id` int(11) NOT NULL,
-  `name` text
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `names`
---
-
-INSERT INTO `names` (`id`, `name`) VALUES
-(1, 'partha'),
-(2, 'samik');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `names`
---
-ALTER TABLE `names`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `names`
---
-ALTER TABLE `names`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+INSERT INTO customers ( id, first_name, last_name, email ) VALUES ( uuid(), 'Partha2 new', 'Chowdhury', 'jane.smith@example.com' );
+INSERT INTO customers ( id, first_name, last_name, email ) VALUES ( uuid(), 'Shamik', 'Ghosh new wer', 'dave.richards@example.com' );
