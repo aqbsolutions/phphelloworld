de68bcdb1f31d5b4815ce732152e68fecb172245
shamik98 <shamik.g@aqbsolutions.com>
2016-09-26 13:12:00 +0530

    Update Customer Table

--- names.sql
+++ names.sql
@@ -7,5 +7,9 @@ CREATE TABLE customers(
   email VARCHAR(64)
 );
 
-INSERT INTO customers ( id, first_name, last_name, email ) VALUES ( uuid(), 'Partha2 new', 'Chowdhury', 'jane.smith@example.com' );
-INSERT INTO customers ( id, first_name, last_name, email ) VALUES ( uuid(), 'Shamik', 'Ghosh new wer', 'dave.richards@example.com' );
+INSERT INTO customers ( id, first_name, last_name, email ) VALUES ( 'E0001', 'Debanjan', 'Sarkar', 'debanjan.s@gmail.com' );
+INSERT INTO customers ( id, first_name, last_name, email ) VALUES ( 'E0002', 'Biswajit', 'Ghosh', 'biswajit.g@gmail.com' );
+INSERT INTO customers ( id, first_name, last_name, email ) VALUES ( 'E0003', 'Partha', 'Chowdhury', 'partha.c@gmail.com' );
+INSERT INTO customers ( id, first_name, last_name, email ) VALUES ( 'E0004', 'Kallol', 'Banerjee', 'kallol.b@gmail.com' );
+INSERT INTO customers ( id, first_name, last_name, email ) VALUES ( 'E0005', 'Ritesh', 'Kumar Singh', 'ritesh.k@gmail.com' );
+INSERT INTO customers ( id, first_name, last_name, email ) VALUES ( 'E0006', 'Shamik', 'Ghosh', 'shamik.g@gmail.com' );
